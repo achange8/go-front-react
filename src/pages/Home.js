1368fc028c5767f8f0118e04994430336f103624
achange8 <95342381+achange8@users.noreply.github.com>
2022-02-27 17:33:59 +0900

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
-function Home() {
+import { useEffect, useState } from "react";
 
-    
+function Home() {
+    const [loading,setloading] = useState(true);
+    const [board,setboard] = useState([]); 
+    const getboard= async() =>{
+        const response = await fetch('http://localhost:8081/');
+        const json = await response.json();
+        setloading(false);
+        console.log(json);
+    };
+    useEffect(()=> {
+        getboard();
+    },[]);
     return(
-        <div> home</div>
+        <div> {loading ? <h1>loading...</h1> : done}</div>
     )
 }
 
