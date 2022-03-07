106ac6040af3a49d853530a141c7364c806ce303
achange8 <95342381+achange8@users.noreply.github.com>
2022-03-07 16:49:18 +0900

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,8 @@
-import {Link} from "react-router-dom"
-function Nav(user){
+import {Link} from "react-router-dom" 
+function Nav({user}){
   let menu;
   if (user===""){
-    
-  }
-
-    return (
-<nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-    <div className="container-fluid">
-      <Link to="/" className="navbar-brand" >Home</Link>
-    </div>
-  
-      <div>
+    menu=(
         <ul className="navbar-nav me-auto mb-2 mb-md-0">
           <li className="nav-item">
             <Link to="/signin"className="nav-link active" aria-current="page" >signin</Link>
@@ -20,7 +11,31 @@ function Nav(user){
            <Link to= "/register" className="nav-link active" aria-current="page">Join</Link>
           </li>
         </ul>
+    );
+  }else{
+    menu=(
+
+        <ul className="navbar-nav me-auto mb-2 mb-md-0">
+          <li className="nav-item">
+            <h3>{user}</h3>
+          </li>
+          <li className="nav-item">
+           <Link to= "/register" className="nav-link active" aria-current="page">log Out</Link>
+          </li>
+        </ul>
+
+    )
+  }
+
+    return (
+<nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <div className="container-fluid">
+      <Link to="/" className="navbar-brand" >Home</Link>
     </div>
+    <div>
+      {menu}
+    </div>
+      
   </nav>
   );
 }
