22887b5fd8e532532ee3877b97b5490616c05203
unknown <achange888@gmail.com>
2022-03-14 03:00:27 +0900

    Update glogin.js

--- src/pages/glogin.js
+++ src/pages/glogin.js
@@ -3,11 +3,17 @@ import { GoogleLogin } from "react-google-login";
 const clientId="295221415874-mmh9djapdgnsl7i8neke5kec0984a9fm.apps.googleusercontent.com";
 
 function Login(){
+    const onSuccess = (res) =>{
+      console.log("Login success! Curent user:",res.profileObj);   
+    }
+    const onFailure = (res) =>{
+      console.log("Login Failed!",res.profileObj);   
+    }
     return (
         <div id="signInButton">
             <GoogleLogin
             clientId={clientId}
-            buttonText="Login"
+            buttonText="Sign in Google"
             onSuccess={onSuccess}
             onFailure={onFailure}
             cookiePolicy={'single_host_origin'}
