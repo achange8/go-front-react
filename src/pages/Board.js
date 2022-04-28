62cc366b62e61b7396507a93bd876b892e3d0edf
achange8 <95342381+achange8@users.noreply.github.com>
2022-04-28 14:57:01 +0900

    time format update
    use moment-react

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,9 +1,12 @@
+import moment from 'moment';
 
 function Board({NUM,TITLE,WRITER,HITCOUNT,CreactAt}){
+    let time  =moment(CreactAt).format('YYYY年MM月DD日 HH:mm');
+    
     
     return(
     <div>
-        <div className="border"> {NUM} : <a href={`/view/${NUM}`}>{TITLE}</a> : {WRITER} : {CreactAt} : {HITCOUNT}</div>
+        <div className="border"> {NUM} : <a href={`/view/${NUM}`}>{TITLE}</a> : {WRITER} : {time} : {HITCOUNT}</div>
     </div>
     )
 }
